6865e72ec5811dd9273ec5f1502b4cba97e4264b
Luke Stone <luke@google.com>
2011-05-24 13:42:20 -0700

    fake login

--- permit/config.py
+++ permit/config.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 """App configuration."""
 config = {}
+config['tipfy.sessions'] = {
+    'secret_key': 'n8c0q38asovnlzisnco'
+    }
--- permit/home.py
+++ permit/home.py
@@ -10,13 +10,31 @@
 """
 from tipfy.app import Response
 from tipfy.handler import RequestHandler
+from tipfy.sessions import SessionMiddleware
 from tipfyext.jinja2 import Jinja2Mixin
 
-
 class HomeHandler(RequestHandler, Jinja2Mixin):
+    middleware = [SessionMiddleware()]
+
     def get(self):
-        """Home Page, no state!."""
+        """Home Page, assumes no state!."""
+        # Get user from session or CGI params.
+        if self.request.args.get('logout'):
+            user = None
+            if 'user' in self.session:
+                del self.session['user']
+        else:
+            user = self.request.args.get('email', None)
+            if user:
+                # set user in session
+                self.session['user'] = user 
+            else:
+                # try to get user from session
+                user = self.session.get('user', None) 
+                
         context = {
             'jurisdiction': 'City of Light',
+            'user': user, 
         }
+
         return self.render_response('home.html', **context)
