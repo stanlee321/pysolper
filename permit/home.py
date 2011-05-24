07d0bd28faab2c240b2cdc1f8480e66fe1f2a8b5
Luke Stone <luke@google.com>
2011-05-24 14:32:33 -0700

    login validates email address, users have roles

--- permit/home.py
+++ permit/home.py
@@ -12,6 +12,8 @@ from tipfy.app import Response
 from tipfy.handler import RequestHandler
 from tipfy.sessions import SessionMiddleware
 from tipfyext.jinja2 import Jinja2Mixin
+import models
+
 
 class HomeHandler(RequestHandler, Jinja2Mixin):
     middleware = [SessionMiddleware()]
@@ -20,21 +22,37 @@ class HomeHandler(RequestHandler, Jinja2Mixin):
         """Home Page, assumes no state!."""
         # Get user from session or CGI params.
         if self.request.args.get('logout'):
-            user = None
-            if 'user' in self.session:
-                del self.session['user']
+            email = None
+            if 'email' in self.session:
+                del self.session['email']
         else:
-            user = self.request.args.get('email', None)
-            if user:
-                # set user in session
-                self.session['user'] = user 
+            email = self.request.args.get('email', None)
+            if email:
+                # set email in session
+                self.session['email'] = email 
             else:
-                # try to get user from session
-                user = self.session.get('user', None) 
+                # try to get email from session
+                email = self.session.get('email', None) 
                 
+        if email:
+            user = models.User.all().filter('email = ', email).get()
+            if not user:
+                user = models.User(email=email)
+                user.put()
+            
+        else: 
+            user = None
+
+        if user:
+            role = self.request.args.get('role', None)
+            if role:
+                user.role = role
+                user.put()
+
         context = {
             'jurisdiction': 'City of Light',
             'user': user, 
+            'roles': models.USER_ROLES,
         }
 
         return self.render_response('home.html', **context)
